63c8ea26afb55804ec5e48ba06cbcdb6f6c661b1
MMD <manuelmoralesdiaz0@gmail.com>
2024-01-05 13:45:38 -0400

    fixing betty

--- monty.c
+++ monty.c
@@ -5,11 +5,10 @@
  * @argc: The number of command-line arguments.
  * @argv: A pointer to an array of strings containing
  * the command-line arguments.
- * Description: print alphabets in lowercase using the putchar
  *
  * Return: returns 0 (Success)
  */
-int main(int argc, char **argv)
+int main(int argc, char **argv[])
 {
 	ssize_t lines_read = 0;
 	size_t n = 0;
