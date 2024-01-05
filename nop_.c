76da314ba195fb426bc2a5a8ef8a4c1c7462f114
MMD <manuelmoralesdiaz0@gmail.com>
2024-01-05 12:46:15 -0400

    changing opcode name

--- nop_.c
+++ nop_.c
@@ -1,11 +1,11 @@
 #include "monty.h"
 
 /**
- * nop_ - Does nothing.
+ * nop - Does nothing.
  * @stack: Double pointer to the stack (top of the stack).
  * @line_number: Line number where the operation is called.
  */
-void nop_(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number)
 {
     (void)stack; // Avoid unused variable warning
     (void)line_number; // Avoid unused variable warning
