ec10e6a19c81aac029f4576fbdfb45653d5b3a60
Mxnu07 <manuelmoralesdiaz0@gmail.com>
2024-01-05 14:13:59 -0400

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -68,3 +68,19 @@ void free_stack(stack_t *head)
 
 	free(head);
 }
+
+/**
+* is_comment - checks the line_strs if it starts with #"
+* Return: 0 or 1
+*/
+int is_comment(void)
+{
+	if (
+		argument->line_strs &&
+		argument->line_strs[0] &&
+		argument->line_strs[0][0] == '#'
+	)
+		return (1);
+
+	return (0);
+}
